7099fbb03e8321a017473aea5b5ab6c417c519fb
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2025-08-27 15:56:08 +0200

    csharp - initial commit

---
+++ csharp/Services/CreditCardService.cs
@@ -0,0 +1,12 @@
+namespace Camunda.Training.CSharp.Services;
+
+public class CreditCardService
+{
+    public void ChargeAmount(string? cardNumber, string? cvc, string? expiryDate, double amount)
+    {
+        Console.Out.WriteLine("Credit card number: " + cardNumber + " CVC: " + cvc + " Expiry date: " + expiryDate +
+                              " Amount to charge: " + amount);
+    }
+}
+
+
