5ed446102947495619bbfa033ca878e762e0ea10
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2026-06-04 00:10:12 +0200

    Apply solution from last exercise

---
+++ java-spring/src/main/java/io/camunda/training/services/CreditCardService.java
@@ -0,0 +1,26 @@
+package io.camunda.training.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreditCardService {
+
+  Logger logger = LoggerFactory.getLogger(CreditCardService.class);
+
+  public void chargeAmount(String cardNumber, String cvc, String expiryDate, Double amount) {
+    if (isExpiryDateValid(expiryDate)) {
+      logger.info("Charging card {} that expires on {} and has a CVC {} with an amount of {} {}",
+              cardNumber, expiryDate, cvc, amount, System.lineSeparator());
+
+      logger.info("Payment completed");
+    } else {
+      throw new IllegalArgumentException("Expiry date invalid: " + expiryDate);
+    }
+  }
+
+  private boolean isExpiryDateValid(String expiryDate) {
+    return expiryDate.length() == 5;
+  }
+}
