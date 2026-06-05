3caf1bd9a33ea870dfaca6555394a37ed4aa2170
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2026-06-05 16:43:24 +0200

    Add solution of last exercise

---
+++ java/src/main/java/io/camunda/training/CamundaApplication.java
@@ -0,0 +1,49 @@
+package io.camunda.training;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.worker.JobWorker;
+import io.camunda.training.services.CreditCardService;
+import io.camunda.training.services.CustomerService;
+import io.camunda.training.workers.CreditCardChargingWorker;
+import io.camunda.training.workers.CreditDeductionWorker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class CamundaApplication {
+
+  private static final Logger logger = LoggerFactory.getLogger(CamundaApplication.class);
+
+  public static void main(String[] args) throws Exception {
+    Properties props = new Properties();
+    try (InputStream in = CamundaApplication.class.getResourceAsStream("/application.properties")) {
+      props.load(in);
+    }
+
+    CreditCardService creditCardService = new CreditCardService();
+    CustomerService customerService = new CustomerService();
+
+    try (CamundaClient client = CamundaClient.newCloudClientBuilder()
+            .withClusterId(props.getProperty("camunda.client.cloud.cluster-id"))
+            .withClientId(props.getProperty("camunda.client.auth.client-id"))
+            .withClientSecret(props.getProperty("camunda.client.auth.client-secret"))
+            .withRegion(props.getProperty("camunda.client.cloud.region"))
+            .build();
+
+         JobWorker creditDeductionWorker = client.newWorker()
+                 .jobType("credit-deduction")
+                 .handler(new CreditDeductionWorker(customerService))
+                 .open();
+
+         JobWorker creditCardWorker = client.newWorker()
+                 .jobType("credit-card-charging")
+                 .handler(new CreditCardChargingWorker(creditCardService))
+                 .open()) {
+      logger.info("Workers started, waiting for jobs...");
+
+      Thread.currentThread().join();
+    }
+  }
+}
---
+++ java/src/main/java/io/camunda/training/services/CreditCardService.java
@@ -0,0 +1,16 @@
+package io.camunda.training.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreditCardService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreditCardService.class);
+
+    public void chargeAmount(String cardNumber, String cvc, String expiryDate, Double amount) {
+        logger.info("Charging card {} that expires on {} and has a CVC {} with an amount of {} {}",
+                cardNumber, expiryDate, cvc, amount, System.lineSeparator());
+
+        logger.info("Payment completed");
+    }
+}
