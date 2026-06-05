a718811b9fe3f1ab3dea606917b1091c2b8a0bc4
hkupitz <hendrik.kupitz@alumni.fh-aachen.de>
2026-06-05 17:58:15 +0200

    Apply solution of last exercise

---
+++ java/src/main/java/io/camunda/training/workers/PaymentCompletionWorker.java
@@ -0,0 +1,35 @@
+package io.camunda.training.workers;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.worker.JobClient;
+import io.camunda.client.api.worker.JobHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PaymentCompletionWorker implements JobHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentCompletionWorker.class);
+
+    private final CamundaClient camundaClient;
+
+    public PaymentCompletionWorker(CamundaClient camundaClient) {
+        this.camundaClient = camundaClient;
+    }
+
+    @Override
+    public void handle(JobClient client, ActivatedJob job) throws Exception {
+        logger.info("Job handled: {}", job.getType());
+
+        String orderId = (String) job.getVariablesAsMap().get("orderId");
+
+        // Tell Order Process to continue
+        camundaClient.newPublishMessageCommand()
+                .messageName("paymentCompletedMessage")
+                .correlationKey(orderId)
+                .send().join();
+
+        // Complete the job in the Payment Process
+        client.newCompleteCommand(job).send();
+    }
+}
